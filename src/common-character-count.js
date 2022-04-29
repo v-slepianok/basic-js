eac99b7a0ee34316b771c70b10743831a0a88797
Valeryia Slepianok <direnok.vd@mail.ru>
2022-04-29 19:36:48 +0300

    feat: solved count-cats, dream-team, common-character

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// function getCommonCharacterCount(/* s1, s2 */) {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+// }
+function getCommonCharacterCount(str1, str2) {
+  let s1 = str1.split('');
+  let s2 = str2.split('');
+  let count = 0;
+
+  for(let  i = 0; i < s1.length; i++){
+    for(let j = 0; j < s2.length; j++){
+      if(s1[i] === s2[j]){
+        count++;
+        s1.splice(i, 1);
+        s2.splice(j, 1);
+        i--;
+      }
+    }
+  }
+  return count;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function createDreamTeam( members ) {
+
+  if(!members || !members.length){
+    return false;
+  }
+  let newArr = [];
+
+  for(let i = 0; i < members.length; i++){
+    if(typeof(members[i]) === 'string'){
+      members[i] = members[i].trim();
+      newArr.push(members[i][0].toUpperCase());
+    }
+  }
+  return newArr.sort().join('');
 }
 
 module.exports = {
